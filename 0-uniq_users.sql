7a5d1fd599d6502c0d4dc13d01234a244b77f672
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-10-12 15:26:04 +0200

    fix: sql script

--- 0-uniq_users.sql
+++ 0-uniq_users.sql
@@ -1,5 +1,5 @@
--- Script that creates a table
-CREATE TABLE  IF NOT EXISTS users (
+-- Script that creates a users table
+CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
