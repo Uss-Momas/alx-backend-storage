6bdc789f0ea18dad0667166a41391b1b14ba047a
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-10-13 14:07:50 +0200

    Fix where clause

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -3,7 +3,7 @@
 DELIMITER $$;
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    SET @average_score = (SELECT avg(score) FROM corrections WHERE user_id = user_id);
+    SET @average_score = (SELECT avg(score) FROM corrections WHERE corrections.user_id = user_id);
     UPDATE users SET average_score=@average_score WHERE id = user_id;
 END
 $$;
