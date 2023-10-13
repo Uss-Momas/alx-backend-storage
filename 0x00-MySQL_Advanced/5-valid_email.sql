fbdf34b00987edec97b9caa40738db4f91fcda70
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-10-13 11:05:31 +0200

    fix: file formatting

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,8 +1,15 @@
 -- create sql script that creates a trigger
 -- the trigger resets the attribute 'valid_email' only when 'email' has been changed
+DELIMETER $$
 CREATE TRIGGER email_validation 
 BEFORE UPDATE
 ON users 
 FOR EACH ROW
-IF NEW.email != OLD.email THEN SET NEW.valid_email = 0;
+BEGIN
+IF NEW.email != OLD.email 
+THEN 
+    SET NEW.valid_email = 0;
 END IF;
+END
+$$
+DELIMITER ;
