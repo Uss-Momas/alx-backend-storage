62bb0343e08f3d2ce83bc639b506bd52e7605a4c
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-10-19 15:18:45 +0200

    fix method of displaying

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -16,14 +16,17 @@ def count(mongo_collection, **filter_args):
 
 
 if __name__ == '__main__':
+    """some stats about Nginx logs"""
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
+
+    count_logs = count(nginx_collection)
+    print(f"{count_logs} logs")
+
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print("{} logs".format(count(nginx_collection)))
     print("Methods:")
     for method in methods:
         count_method = count(nginx_collection, method=method)
-        print("\tmethod {}: {}".format(method, count_method))
-    data = nginx_collection.find()
-    print("{} status check".format(count(nginx_collection, method='GET',
-                                         path='/status')))
+        print(f"\tmethod {method}: {count_method}")
+    status_ch = count(nginx_collection, method='GET', path='/status')
+    print(f"{status_ch} status check")
