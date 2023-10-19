d9c31079b1089f9a34a9c5e0c9b6c2a17fcc781c
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-10-19 13:51:58 +0200

    return the schols with specific topics

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 """
-module documentation
+filter schools by topic module
 """
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    returns the list of school having a specific topic:
+    """
+    return mongo_collection.find({'topics': {'$in':  [topic]}})
