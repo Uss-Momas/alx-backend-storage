0e7c52b819f7b0ea93615503ab3fd4c27a2dea6d
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-10-19 11:25:46 +0200

    python method to create the list of all documents in a collection

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 """
-module documentation
+all module
+retrieves data from mongoDB
 """
+
+
+def list_all(mongo_collection):
+    """
+    function to list all documents in a collection
+    """
+    return list(mongo_collection.find())
