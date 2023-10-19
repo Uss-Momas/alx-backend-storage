6e450dc147722db5200ae3bc507d38d89e0ea4c2
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-10-19 15:11:46 +0200

    showing the stats of logs db

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
 """
-module documentation
+logs stats module
 """
+from pymongo import MongoClient
+
+
+def count(mongo_collection, **filter_args):
+    """
+    counts the number of documents of a collection
+    Args:
+        mongo_collection - pymongo collection cursor
+        filter_args - a dictionaire of named variables params
+    """
+    return mongo_collection.count_documents(filter_args)
+
+
+if __name__ == '__main__':
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("{} logs".format(count(nginx_collection)))
+    print("Methods:")
+    for method in methods:
+        count_method = count(nginx_collection, method=method)
+        print("\tmethod {}: {}".format(method, count_method))
+    data = nginx_collection.find()
+    print("{} status check".format(count(nginx_collection, method='GET',
+                                         path='/status')))
