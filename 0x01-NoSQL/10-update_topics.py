331faf1501a0d3c1bef9e8d6d8bc0e1f45e5787d
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-10-19 11:52:17 +0200

    updates all documents

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 """
-module documentation
+update module
 """
+
+
+def update_topics(mongo_collection, name, topics):
+    """ function that changes all topics of a school document based on the name
+    Args:
+        mongo_collection - pymongo collection object
+        name - string of the school name to update
+        topics - list of strings, will be the list of topics approached in the
+                school
+    """
+    mongo_collection.update_many({"name": name}, {'$set': {'topics': topics}})
