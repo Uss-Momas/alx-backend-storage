451db04f56a0ac3fcb62a9c9a5a7bf36f49a6f88
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-10-19 11:36:17 +0200

    fix the right pymongo version to insert

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -10,6 +10,7 @@ def insert_school(mongo_collection, **kwargs):
         mongo_collection - the target collection
         kwargs - the data to be inserted in the mongo_collection
     """
-    _id = mongo_collection.insert_one(kwargs)
-    print(_id)
+    # _id = mongo_collection.insert_one(kwargs)
+    # for the version in the ALX checker server
+    _id = mongo_collection.insert(kwargs)
     return _id
