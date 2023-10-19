dfb3d0e4528232c8fdba857845b1c11c9a8185f5
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2023-10-19 11:33:21 +0200

    python function to insert a  new document to a collection

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 """
-module documentation
+insert module
 """
+
+
+def insert_school(mongo_collection, **kwargs):
+    """inserts a new document in a collection
+    Arguments:
+        mongo_collection - the target collection
+        kwargs - the data to be inserted in the mongo_collection
+    """
+    _id = mongo_collection.insert_one(kwargs)
+    print(_id)
+    return _id
